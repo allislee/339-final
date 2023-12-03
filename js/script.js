52e6d158f3560098b8fe7b8229ccc9a7fc744a41
allislee <123308102+allislee@users.noreply.github.com>
2023-12-03 00:32:16 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var tomatoCard = document.querySelector('.health_info1 .scene:nth-child(1) .card');
+var eggCard = document.querySelector('.health_info1 .scene:nth-child(2) .card');
+var tofuCard = document.querySelector('.health_info2 .scene:nth-child(1) .card');
+var garlicCard = document.querySelector('.health_info2 .scene:nth-child(2) .card');
+var bcCard = document.querySelector('.health_info3 .scene:nth-child(1) .card');
+var mushCard = document.querySelector('.health_info3 .scene:nth-child(2) .card');
+
+// Add event listeners for each card
+tomatoCard.addEventListener('click', function () {
+  tomatoCard.classList.toggle('is-flipped');
+});
+
+eggCard.addEventListener('click', function () {
+  eggCard.classList.toggle('is-flipped');
+});
+
+tofuCard.addEventListener('click', function () {
+  tofuCard.classList.toggle('is-flipped');
+});
+
+garlicCard.addEventListener('click', function () {
+  garlicCard.classList.toggle('is-flipped');
+});
+
+bcCard.addEventListener('click', function () {
+  bcCard.classList.toggle('is-flipped');
+});
+
+mushCard.addEventListener('click', function () {
+  mushCard.classList.toggle('is-flipped');
+});
---
+++ js/tippy.js
@@ -0,0 +1,11 @@
+tippy('.drinks', {
+    content: 'Come learn about some drinks you can make!',
+  });
+
+tippy('.recipe_link', {
+content: 'Click to get a link to a recipe!',
+});
+
+tippy('.card', {
+    content: 'Click to learn about some of the health benefits!',
+    });
