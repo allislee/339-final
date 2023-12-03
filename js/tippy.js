4a372a76b369fe759372ab39d5cab5145f266a20
allislee <123308102+allislee@users.noreply.github.com>
2023-12-03 01:20:12 -0500

    Add files via upload

--- js/tippy.js
+++ js/tippy.js
@@ -9,3 +9,19 @@ content: 'Click to get a link to a recipe!',
 tippy('.card', {
     content: 'Click to learn about some of the health benefits!',
     });
+
+tippy('.duck', {
+  content: 'Hello! :D',
+  });
+
+tippy('.meals', {
+  content: 'Go learn about some yummy meals you can make!',
+  });
+
+tippy('.home', {
+  content: 'Go learn about the website!',
+  });
+
+tippy('.current', {
+  content: 'You are currently here!',
+  });
